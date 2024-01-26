82f7c352dd91251f9844670b9d2203cb37ea381a
Sokim Nam <namsokim9@gmail.com>
2024-01-25 23:49:35 -0600

    finish up the frontend

--- public/profile/src/app/about/about.component.ts
+++ public/profile/src/app/about/about.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   headingDescription: string = 'About Me';
   description: string =
-    "Im a passionate and results-driven software developer with a strong background in C# and the .NET ecosystem. With over 3 years of hands-on experience, I've had the opportunity to work on a diverse range of projects and technologies, honing my skills to deliver top-notch solutions.";
+    "I'm a passionate and results-driven software developer with a strong background in C# and the .NET ecosystem. With over 3 years of hands-on experience, I've had the opportunity to work on a diverse range of projects and technologies, honing my skills to deliver top-notch solutions.";
+  experience: string =
+    'Leveraged 3+ years of hands-on experience as a driven Software Engineer to design and implement web-based applications. Collaborated closely with cross-functional teams and participating in design, ensuring efficient client-server communication in Agile/Scrum environment.';
 }
